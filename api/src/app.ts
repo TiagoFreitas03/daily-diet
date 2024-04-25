727d2dafa89de72c5c427df36a6578b6c43a791d
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2024-04-25 19:58:23 -0300

    chore: add route to create users

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,12 @@
 import fastify from 'fastify'
 
+import { usersRoutes } from './routes/users'
+
 const app = fastify()
+
+app.register(usersRoutes, {
+  prefix: 'users',
+})
 
 app
   .listen({
---
+++ api/src/routes/users.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+
+import { database } from '../database'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+
+    const { name, email } = createUserBodySchema.parse(request.body)
+
+    await database('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+    })
+
+    return reply.status(201).send()
+  })
+}
